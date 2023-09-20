556c96a44b4f0479b638a5acdfcbb737b6b2eb0b
tangrun <1821047024@qq.com>
2023-09-20 14:00:35 +0800

    添加字段sql合并；

--- broker/migrate/mysql/V53__add_messages_ip_region_info.sql
+++ broker/migrate/mysql/V53__add_messages_ip_region_info.sql
@@ -21,116 +21,151 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
-alter table `t_messages` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_0` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_1` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_2` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_3` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_4` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_5` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_6` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_7` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_8` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_9` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_10` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_11` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_12` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_13` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_14` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_15` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_16` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_17` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_18` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_19` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_20` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_21` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_22` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_23` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_24` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_25` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_26` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_27` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_28` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_29` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_30` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_31` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_32` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_33` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_34` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-alter table `t_messages_35` add column `_remote_address_ip` varchar(48) DEFAULT NULL;
-
-alter table `t_messages` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_0` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_1` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_2` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_3` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_4` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_5` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_6` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_7` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_8` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_9` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_10` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_11` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_12` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_13` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_14` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_15` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_16` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_17` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_18` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_19` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_20` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_21` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_22` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_23` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_24` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_25` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_26` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_27` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_28` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_29` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_30` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_31` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_32` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_33` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_34` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-alter table `t_messages_35` add column `_remote_address_port` varchar(8) DEFAULT NULL;
-
-alter table `t_messages` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_0` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_1` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_2` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_3` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_4` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_5` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_6` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_7` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_8` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_9` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_10` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_11` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_12` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_13` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_14` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_15` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_16` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_17` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_18` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_19` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_20` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_21` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_22` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_23` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_24` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_25` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_26` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_27` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_28` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_29` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_30` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_31` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_32` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_33` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_34` add column `_remote_address_region` varchar(64) DEFAULT NULL;
-alter table `t_messages_35` add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_0`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_1`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_2`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_3`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_4`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_5`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_6`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_7`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_8`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_9`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_10`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_11`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_12`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_13`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_14`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_15`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_16`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_17`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_18`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_19`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_20`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_21`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_22`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_23`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_24`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_25`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_26`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_27`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_28`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_29`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_30`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_31`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_32`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_33`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_34`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
+alter table `t_messages_35`
+    add column `_remote_address_ip` varchar(48) DEFAULT NULL,
+    add column `_remote_address_port` varchar(8) DEFAULT NULL,
+    add column `_remote_address_region` varchar(64) DEFAULT NULL;
